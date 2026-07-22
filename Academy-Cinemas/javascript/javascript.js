9f1483dba912cc23fe4ffd63f873d631bce74822
Nicholas Martin <57820367+nmartin2600@users.noreply.github.com>
2026-07-22 17:55:42 -0400

    Implement toast popup window for tickets section

--- Academy-Cinemas/javascript/javascript.js
+++ Academy-Cinemas/javascript/javascript.js
@@ -10,3 +10,31 @@ popoverTriggerList.forEach(function(element) {
         trigger: 'hover'
     });
 });
+
+// Initialize Toast
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl)
+})
+
+// Function to display toast with selected options
+function displaySelectedMovieOptions() {
+    var movie = document.getElementById('movieSelect')
+        .options[document.getElementById('movieSelect').selectedIndex].text;
+    var time = document.getElementById('timeSelect')
+        .options[document.getElementById('timeSelect').selectedIndex].text;
+    var quantity = document.getElementById('quantity').value;
+
+    var message = "Purchase confirmed for: " + movie + "\nTime: "
+        + time + "\nTickets: " + quantity;
+    
+    // Display toast
+    var toastBody = document.getElementById('toastBody');
+    toastBody.textContent = message;
+    var toast = new bootstrap.Toast(document.getElementById('toastDisplay'));
+    toast.show();
+}
+
+function buyTickets() {
+    displaySelectedMovieOptions();
+}
